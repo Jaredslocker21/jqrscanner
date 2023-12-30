50792c936f40a445c6efc083c81dcc5cccbf53af
Jaredslocker21 <jaredslocker21@gmail.com>
2023-12-30 13:11:54 +0000

    changed result container result

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let shouldStoreData = true;
+
 async function storeData(qrData) {
   shouldStoreData = false;
+
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
@@ -18,78 +20,86 @@ async function storeData(qrData) {
   fetch("https://jqrscanner-production.up.railway.app/addData", requestOptions)
     .then(response => response.text())
     .then(result => {
-      console.log(result)
-      alert("saved")
-      shouldStoreData = true
+      console.log(result);
+      alert("saved");
+      shouldStoreData = true;
     })
     .catch(error => console.log('error', error));
 }
 
 function displayInfo(label, value) {
-let resultElement = document.getElementById('result');
-resultElement.innerHTML += `<p><strong>${label}:</strong> ${value}</p>`;
+  let resultList = document.getElementById('resultList');
+
+  // Create a new span element
+  let resultItem = document.createElement('span');
+
+  // Set the content of the span
+  resultItem.innerHTML = `<p><strong>${label}:</strong> ${value}</p>`;
+
+  // Add the new span at the beginning of the list
+  resultList.insertBefore(resultItem, resultList.firstChild);
 }
+
 
 async function onScanSuccess(qrCodeMessage) {
-const vcardSplit = qrCodeMessage.split("\n");
+  const vcardSplit = qrCodeMessage.split("\n");
 
-let firstName = "";
-let lastName = "";
-let company = "";
-let email = "";
+  let firstName = "";
+  let lastName = "";
+  let company = "";
+  let email = "";
 
-for (let index in vcardSplit) {
-  let partInfo = vcardSplit[index].trim();
-  const infoSplit = partInfo.split(":");
+  for (let index in vcardSplit) {
+    let partInfo = vcardSplit[index].trim();
+    const infoSplit = partInfo.split(":");
 
-  if (infoSplit.length >= 2) {
-    const key = infoSplit[0].trim();
-    const value = infoSplit.slice(1).join(":").trim();
+    if (infoSplit.length >= 2) {
+      const key = infoSplit[0].trim();
+      const value = infoSplit.slice(1).join(":").trim();
 
-    console.log("Key:", key);
-    console.log("Value:", value);
+      console.log("Key:", key);
+      console.log("Value:", value);
 
-    if (key === "N;CHARSET=UTF-8") {
-      console.log("Processing Name:", value);
-      const nameSplit = value.split(";");
-      console.log("Name Split:", nameSplit);
-      if (nameSplit.length >= 2) {
-        firstName = nameSplit[1];
-        lastName = nameSplit[0];
-      } else {
-        lastName = nameSplit[0];
+      if (key === "N;CHARSET=UTF-8") {
+        console.log("Processing Name:", value);
+        const nameSplit = value.split(";");
+        console.log("Name Split:", nameSplit);
+        if (nameSplit.length >= 2) {
+          firstName = nameSplit[1];
+          lastName = nameSplit[0];
+        } else {
+          lastName = nameSplit[0];
+        }
+      } else if (key === "ORG") {
+        company = value;
+      } else if (key === "EMAIL") {
+        email = value;
+      } else if (key.startsWith("ORG")) {
+        company = value;
       }
-    } else if (key === "ORG") {
-      company = value;
-    } else if (key === "EMAIL") {
-      email = value;
-    }
-    else if (key.startsWith("ORG")) {
-    company = value;
     }
   }
-}
 
-// Display relevant information
-displayInfo("First Name", firstName);
-displayInfo("Last Name", lastName);
-displayInfo("Company", company);
-displayInfo("Email", email);
+  // Display relevant information
+  displayInfo("First Name", firstName);
+  displayInfo("Last Name", lastName);
+  displayInfo("Company", company);
+  displayInfo("Email", email);
 
-// Save the data to a Google Sheet
-if (shouldStoreData) {
-  await storeData([firstName + " " + lastName, company, email]);
-}
+  // Save the data to a Google Sheet
+  if (shouldStoreData) {
+    await storeData([firstName + " " + lastName, company, email]);
+  }
 
-// Log information to the console for testing
-console.log("First Name:", firstName);
-console.log("Last Name:", lastName);
-console.log("Company:", company);
-console.log("Email:", email);
+  // Log information to the console for testing
+  console.log("First Name:", firstName);
+  console.log("Last Name:", lastName);
+  console.log("Company:", company);
+  console.log("Email:", email);
 }
 
 function onScanError(errorMessage) {
-  //handle scan error
+  // handle scan error
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
